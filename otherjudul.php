133aa9b29e28fc5b21aee6e6a2a832f49c46b304
fredywindana <67693091+fredywindana@users.noreply.github.com>
2023-07-16 13:26:48 +0700

    Upload Other Judul
    Other Judul

---
+++ otherjudul.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>APlikasi Other Penawaran</title>
+</head>
+<body>
+	<p align="center">Contoh Judul</p>
+</body>
+</html>
